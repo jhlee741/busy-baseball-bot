b31ad1a3d36d2632409f004110df3416041e35cf
John Lee <jhlee90741@gmail.com>
2023-02-11 11:48:15 -0500

    test files for understanding pybaseball

---
+++ main.py
@@ -0,0 +1,16 @@
+from pybaseball import statcast
+from pybaseball import statcast_batter
+from pybaseball import playerid_lookup
+import pandas as pd
+
+playerid_lookup('bichette','bo')
+
+
+start_dt = '2022-09-01'
+end_dt = '2022-09-30'
+team = 'TOR'
+
+
+output = statcast(start_dt, end_dt, team)
+output.to_csv('Pitching.csv', index = False)
+# output.to_csv('D:\Documents\Projects\BaseballBot\\test.csv', index = False)
